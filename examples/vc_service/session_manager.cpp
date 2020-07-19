6c023abef82ba520f0506201f707a7496348fd51
raoyonghui <raoyonghui@bytedance.com>
2020-07-19 22:55:25 +0800

    merge

--- examples/vc_service/session_manager.cpp
+++ examples/vc_service/session_manager.cpp
@@ -30,8 +30,8 @@ bool SessionManager::AddSession(string session_id,
     return false;
   }
   session_map[session_id] = session;
-  printf("AddSession succeed, id:%s,there is %d sessions now\n", session_id.c_str(),
-         session_map.size());
+  printf("AddSession succeed, id:%s,there is %d sessions now\n",
+         session_id.c_str(), session_map.size());
   return true;
 }
 
@@ -45,5 +45,31 @@ bool SessionManager::ClearSession(string session_id) {
     return true;
   }
   printf("ClearSession failed: session not exist, id:%s\n", session_id.c_str());
+  return false;
+}
+
+bool SessionManager::AddThread(string session_id, std::thread &thread) {
+  std::unique_lock<std::mutex> lck(thread_mutex);
+  if (send_threads.count(session_id)) {
+    printf("AddThread failed: thread alreay exist, id:%s\n",
+           session_id.c_str());
+    return false;
+  }
+  send_threads[session_id] = thread;
+  printf("AddThread succeed, id:%s,there is %d threads now\n",
+         session_id.c_str(), send_threads.size());
+  return true;
+}
+
+bool SessionManager::ClearThread(std::thread &thread) {
+  std::unique_lock<std::mutex> lck(thread_mutex);
+  auto iter = send_threads.find(session_id);
+  if (iter != send_threads.end()) {
+    send_threads.erase(iter);
+    printf("ClearThread succeed, id:%s, there is %d threads now\n",
+           session_id.c_str(), send_threads.size());
+    return true;
+  }
+  printf("ClearThread failed: session not exist, id:%s\n", session_id.c_str());
   return false;
 }
