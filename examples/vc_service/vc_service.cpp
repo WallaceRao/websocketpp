23b0bb8228e6bd4bd5132e6a76b6b1f88da3c7b9
raoyonghui <raoyonghui@bytedance.com>
2020-07-20 00:19:51 +0800

    merge

--- examples/vc_service/vc_service.cpp
+++ examples/vc_service/vc_service.cpp
@@ -156,7 +156,7 @@ void on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg) {
   if (new_session) {
     // start a thread to send back response
     session->StartProcess();
-    std::thread thread_for_send = std::thread(&send_thread, s, con);
+    std::thread thread_for_send(&send_thread, s, con);
     session_manager->AddThread(connection_id, thread_for_send);
   }
   cout << "a request has been processed to connection id:" << connection_id
