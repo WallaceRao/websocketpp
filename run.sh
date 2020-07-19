cd6a57cb0ed8166f75c17c0f00193ce8100fb96a
EC2 Default User <ec2-user@ip-172-31-5-88.ap-east-1.compute.internal>
2020-07-19 09:16:54 +0000

    add file

---
+++ run.sh
@@ -0,0 +1,2 @@
+export LD_LIBRARY_PATH=/home/ec2-user/vc_service/jsoncpp/build/lib:/home/ec2-user/libtorch/libtorch/lib:/home/ec2-user/vc_engine/vc_demo/build:${LD_LIBRARY_PATH}
+./build/bin/vc_serviced
